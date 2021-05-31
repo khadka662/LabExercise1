85e1070530cd369ba01ada7f1bca9ba5ce6a1d2d
ASUS <khadkasagar290@gmail.com>
2021-05-31 11:44:23 +0545

    initial commit

---
+++ FirstProject1/first.py
@@ -0,0 +1,4 @@
+a=10
+b=a
+c=a+b
+print(b)
---
+++ LabExercise1/LabOne/one.py
@@ -0,0 +1,8 @@
+# Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
+
+num1 = int(input("enter the first value: "))
+num2 = int(input("enter the second value: "))
+num3 = int(input("enter the third value: "))
+sum = num1+num2+num3
+print(sum)
+print(f"The sum of fiven three numbers is {sum}")
