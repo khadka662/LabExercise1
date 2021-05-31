50a449740876eb454b15f7f0b2979957fbd07e8b
ASUS <khadkasagar290@gmail.com>
2021-05-31 15:32:23 +0545

    commit

--- LabExercise1/LabOne/three.py
+++ LabExercise1/LabOne/three.py
@@ -2,4 +2,9 @@
 # The remaining (the undivisible) part remains in the basket. How many apples will each single student get? How many apples will remain in the basket? The program reads the numbers N and K.
 # It should print the two answers for the questions above.
 
-N1 = int(input("
+N = int(input("Enter the number of student: "))
+K = int(input("Enter the number of apples: "))
+distribute = K//N
+remaining = K%N
+print(f"the number of apple student  gets is{distribute} ")
+print(f"the number of apple remains is {remaining}")
