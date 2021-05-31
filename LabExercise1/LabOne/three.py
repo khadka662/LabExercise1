e1dc6ff112db6ad48d07e44c1a1331d402a34b70
ASUS <khadkasagar290@gmail.com>
2021-05-31 12:33:32 +0545

    commit

--- LabExercise1/LabOne/three.py
+++ LabExercise1/LabOne/three.py
@@ -1,3 +1,5 @@
 #N students take K apples and distribute them among each other evenly.
 # The remaining (the undivisible) part remains in the basket. How many apples will each single student get? How many apples will remain in the basket? The program reads the numbers N and K.
 # It should print the two answers for the questions above.
+
+N1 = int(input("
---
+++ LabExercise1/LabOne/two.py
@@ -0,0 +1,8 @@
+# write a program that reads the length of the base and the height of a right-angled triangle and print the area. Every number is given o
+base = int(input("enter the value of base: "))
+height = int(input("enter the value of height: "))
+area = (base*height)//2
+print(area)
+print(f"The area of right-angled triangle is{area}")
+
+
