9707759394450c6647d106e9128f92ed31921428
ASUS <khadkasagar290@gmail.com>
2021-05-31 12:11:24 +0545

    commit

--- LabExercise1/LabOne/one.py
+++ LabExercise1/LabOne/one.py
@@ -1,8 +1,18 @@
 # Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
+# write a program that reads the length of the base and the height of a right-angled triangle and print the area. Every number is given on a separate line.
 
 num1 = int(input("enter the first value: "))
 num2 = int(input("enter the second value: "))
 num3 = int(input("enter the third value: "))
 sum = num1+num2+num3
 print(sum)
-print(f"The sum of fiven three numbers is {sum}")
+print(f"The sum of given three numbers is {sum}")
+
+'''
+# write a program that reads the length of the base and the height of a right-angled triangle and print the area. Every number is given o
+base = int(input("enter the value of base: "))
+height = int(input("enter the value of height: "))
+area = (base*height)//2
+print(area)
+print(f"The area of right-angled triangle is{area}")1
+'''
