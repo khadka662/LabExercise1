40f12511e640da87adbe875927f0ed499ec1d85e
ASUS <khadkasagar290@gmail.com>
2021-06-07 11:21:44 +0545

    substraction

---
+++ FifthProject4/main.py
@@ -0,0 +1,7 @@
+#Write a program to find a area of triangle.
+
+base = int(input("enter the value of base: "))
+height = int(input("enter the value of height: "))
+area = (base*height)//2
+print(f"The {area} of  triangle is{area}")
+
